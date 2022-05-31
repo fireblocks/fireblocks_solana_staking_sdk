4d138625301e9e07cada61f623459d463d43f233
Slava Serebrianniy <slava@fireblocks.com>
2022-05-31 21:41:46 +0000

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,15 +3,18 @@ import path from "path";
 import { FireblocksSDK } from "fireblocks-sdk";
 import { SolStaker } from "./src/sol-staker"
 
-
+//Provide the API key and the path to the RSA secret key
 const apiSecret = fs.readFileSync(path.resolve(__dirname, '<path_to_your_secret_key>'), "utf8"); 
 const apiKey = "<your_fb_api_key>"
-const fireblocks = new FireblocksSDK(apiSecret, apiKey);
 
+//Set to false if you wish to work on mainnet
 const DEVNET: boolean = true;
+
+//Set your staking vault account ID here
 const VAULT_ACCOUNT: any = '<your_vault_account_id>';
 
 
+const fireblocks = new FireblocksSDK(apiSecret, apiKey);
 const solStaker = new SolStaker(fireblocks, VAULT_ACCOUNT, DEVNET);
 
 (async() => {
